731fca70be2900725152ac88fb79256992f7f8c9
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-13 15:34:04 -0600

    Set  async function to create a game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles.css';
+
+async function createNewGame(gameName) {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name: gameName }),
+  });
+
+  const result = await response.json();
+  return result;
+}
+
+(async () => {
+  const gameName = 'My Cool New Game';
+  const game = await createNewGame(gameName);
+  const gameId = game.result.split(': ')[1];
+  console.log('Game ID:', gameId);
+})();
